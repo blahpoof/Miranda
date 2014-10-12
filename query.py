c57056a89261f2f8116fb373731e6f0ce5603e76
Andrew Huang <andrewh.huang@mail.utoronto.ca>
2014-10-12 16:58:23 -0400

    Comment frontend

--- query.py
+++ query.py
@@ -4,46 +4,30 @@ history = {} # Dictionary storing number of times each word has been searched si
 
 @route('/')
 def query():
-	''' Uses http GET method to get keywords from user via an HTML form. '''
+	''' If a keyword string has been searched, returns an HTML table displaying the word count of the string, and another
+		HTML table displaying the top 20 most searched words. Otherwise, returns an HTML form requesting a keyword string. '''
 
 	keywords = request.GET.get('keywords', '').strip()
-	if keywords:
-		query = {}
+
+	if keywords: # A keyword string has been submitted
+		query = {} # Dictionary storing word count of keywords string 
 		words = keywords.lower().strip().split() # list of keywords in lowercase form
+		
 		for word in words:
-			# Add word to dictionaries, or plus one if already in dictionary
+			# Add word to dictionaries, or increase count by 1 if already in dictionary
 			history[word] = history.get(word, 0) + 1
 			query[word] = query.get(word, 0) + 1
 
-		sorted_history = sorted(history.items(), key=lambda x:x[1], reverse=True ) # list of (key, value) of history sorted by word count
-		return template("keywords", d=query, l=sorted_history[:20]) # http POST tables using HTML template
-	else:
+		sorted_history = sorted(history.items(), key=lambda x:x[1], reverse=True ) # list of (word, count) of history sorted by count
+		return template("keywords", d=query, l=sorted_history[:20]) # display tables using HTML template
+	
+	else: # A keyword string has not been submitted
 		return '''
 			<form action='/', method="GET">
 				Keyword: <input name="keywords" type="text"/>
 				<input value="Submit" type="submit"/>
 			</form>
 		''' 
-
-# @route('/')
-# def print_query():
-# 	''' Displays word count of keyword search in an HTML table, and a list of the top 20 most searched words in another HTML table using http POST '''
-
-# 	# keywords = request.forms.get('keywords') # Get keywords from an HTML form 
-# 	# return "test"
-# 	# keywords = url[10:]
-# 	keywords = request.query.one
-# 	query = {} # Dictionary storing word count of current search
-# 	if keywords:
-# 		words = keywords.lower().strip().split() # list of keywords in lowercase form
-# 		for word in words:
-# 			# Add word to dictionaries, or plus one if already in dictionary
-# 			history[word] = history.get(word, 0) + 1
-# 			query[word] = query.get(word, 0) + 1
-
-# 	sorted_history = sorted(history.items(), key=lambda x:x[1], reverse=True ) # list of (key, value) of history sorted by word count
-# 	if query and keywords:
-# 		return template("keywords", d=query, l=sorted_history[:20]) # http POST tables using HTML template
 	
 run(host='localhost', port=8080, debug=True, reloader=True)
 
